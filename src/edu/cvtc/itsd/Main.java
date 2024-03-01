a8ecd95f82810b591e95ffb559ad3e9b0fb984c8
Harley Carbon <hcarbon@student.cvtc.edu>
2024-03-01 16:59:10 -0600

    Change color scheme for all panels

--- src/edu/cvtc/itsd/Main.java
+++ src/edu/cvtc/itsd/Main.java
@@ -9,6 +9,7 @@ import java.util.TimerTask;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.text.*;
+import java.awt.Color;
 
 // CiCo application's primary class ///////////////////////////////////////////
 public class Main {
@@ -240,13 +241,13 @@ public class Main {
     panelMain.setMinimumSize(new Dimension(320, 240));
     panelMain.setPreferredSize(new Dimension(640, 480));
     panelMain.setMaximumSize(new Dimension(640, 480));
-    panelMain.setBackground(Color.black);
+    panelMain.setBackground(new Color(114,90,35));
 
     panelMain.add(Box.createVerticalGlue());
     JLabel labelDirective = new JLabel("Scan card", JLabel.LEADING);
     labelDirective.setFont(fontMain);
     labelDirective.setAlignmentX(JComponent.CENTER_ALIGNMENT);
-    labelDirective.setForeground(Color.cyan);
+    labelDirective.setForeground(new Color(219,208,181));
     panelMain.add(labelDirective);
 
     fieldNumber = new JTextField();
@@ -255,14 +256,14 @@ public class Main {
     fieldNumber.setPreferredSize(new Dimension(200, 32));
     fieldNumber.setMaximumSize(new Dimension(200, 32));
     fieldNumber.setAlignmentX(JComponent.CENTER_ALIGNMENT);
-    fieldNumber.setBackground(Color.green);
-    fieldNumber.setForeground(Color.magenta);
+    fieldNumber.setBackground(new Color(172,159,130));
+    fieldNumber.setForeground(new Color(78,60,16));
     panelMain.add(fieldNumber);
 
     JButton updateButton = new JButton("Update");
     updateButton.setAlignmentX(JComponent.CENTER_ALIGNMENT);
     updateButton.addActionListener(new Update());
-    updateButton.setForeground(Color.green);
+    updateButton.setForeground(new Color(78,60,16));
     panelMain.add(updateButton);
 
     panelMain.add(Box.createVerticalGlue());
@@ -273,19 +274,19 @@ public class Main {
     panelStatus.setMinimumSize(new Dimension(320, 240));
     panelStatus.setPreferredSize(new Dimension(640, 480));
     panelStatus.setMaximumSize(new Dimension(640, 480));
-    panelStatus.setBackground(Color.blue);
+    panelStatus.setBackground(new Color(114,90,35));
 
     panelStatus.add(Box.createVerticalGlue());
     labelUser = new JLabel("Registrant", JLabel.LEADING);
     labelUser.setFont(fontMain);
     labelUser.setAlignmentX(JComponent.CENTER_ALIGNMENT);
-    labelUser.setForeground(Color.yellow);
+    labelUser.setForeground(new Color(219,208,181));
     panelStatus.add(labelUser);
 
     labelState = new JLabel("updated", JLabel.LEADING);
     labelState.setFont(fontMain);
     labelState.setAlignmentX(JComponent.CENTER_ALIGNMENT);
-    labelState.setForeground(Color.magenta);
+    labelState.setForeground(new Color(78,60,16));
     panelStatus.add(labelState);
 
     panelStatus.add(Box.createVerticalGlue());
@@ -293,7 +294,7 @@ public class Main {
     buttonNext = new JButton("Next");
     buttonNext.addActionListener(new Handler());
     buttonNext.setAlignmentX(JComponent.CENTER_ALIGNMENT);
-    buttonNext.setForeground(Color.green);
+    buttonNext.setForeground(new Color(78,60,16));
     panelStatus.add(buttonNext);
 
     // Error panel ////////////////////////////////////////////////////////////
@@ -302,19 +303,19 @@ public class Main {
     panelError.setMinimumSize(new Dimension(320, 240));
     panelError.setPreferredSize(new Dimension(640, 480));
     panelError.setMaximumSize(new Dimension(640, 480));
-    panelError.setBackground(Color.red);
+    panelError.setBackground(new Color(114,90,35));
 
     panelError.add(Box.createVerticalGlue());
     labelReason = new JLabel("", JLabel.LEADING);
     labelReason.setFont(fontMain);
     labelReason.setAlignmentX(JComponent.CENTER_ALIGNMENT);
-    labelReason.setForeground(Color.yellow);
+    labelReason.setForeground(new Color(219,208,181));
     panelError.add(labelReason);
 
     buttonAcknowledge = new JButton("OK");
     buttonAcknowledge.addActionListener(handler);
     buttonAcknowledge.setAlignmentX(JComponent.CENTER_ALIGNMENT);
-    buttonAcknowledge.setForeground(Color.red);
+    buttonAcknowledge.setForeground(new Color(78,60,16));
     panelError.add(buttonAcknowledge);
     panelError.add(Box.createVerticalGlue());
 
